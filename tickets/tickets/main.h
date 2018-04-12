d5f69c55fc41c20e63880a55cc0bb4de37e1abf2
MarcelUstowski <38075549+MarcelUstowski@users.noreply.github.com>
2018-04-12 20:23:24 +0100

    Add files via upload

---
+++ tickets/tickets/main.h
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+
+using namespace std;
+
+//multipying
+	int biletulgowy(int a , int b);
+
+//multipying
+	int biletynormalny(int d , int c);
+//absolute value out of "reszta";
+	int reszta1(int reszta);
+
+//calculates % discount
+	int rabat1(int procent , int razem);
+
+//random number
+	int losowanie(int y);
+
+//calculates the change in coins
+	int monety(int reszta);
+
+
+//prints amount of tickets
+	 int bilety(int ulg , int norm);
